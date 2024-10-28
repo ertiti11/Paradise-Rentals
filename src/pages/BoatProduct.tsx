3f920511270d024e6105c52821f51da47359d3d4
AlvaroGarcia0093 <agm0093@alu.medac.es>
2024-10-28 17:26:30 +0100

    test

--- src/pages/BoatProduct.tsx
+++ src/pages/BoatProduct.tsx
@@ -12,6 +12,7 @@ const BoatProduct = () => {
 
   return (
     <div className="container mx-auto py-16">
+      <h1>test</h1>
       <div className="bg-white rounded-lg shadow-lg overflow-hidden">
         <div className="md:flex">
           <div className="md:flex-shrink-0">
