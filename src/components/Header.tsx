46fbe768e28fd1c88c69287c62dee98667f3a7d5
AlvaroGarcia0093 <agm0093@alu.medac.es>
2024-10-28 19:10:39 +0100

    navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,59 +1,27 @@
-import { useState } from 'react';
-import './Navbar.css'; // Estilos personalizados para animaciones y blur
-import Logo from '../assets/logo.svg'; // Logo de la empresa
-import { NavLink } from 'react-router-dom';
+import logo from '../assets/Logo.svg';
+
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <>
-      {/* Barra de navegación superior visible siempre */}
-      <div className={`fixed top-0 left-0 w-full bg-white/20 shadow-md z-50`}>
-        <div className="flex items-center justify-between px-6 py-4">
-          {/* Logo (opcional) */}
-          <NavLink to="/" className="text-4xl text-black font-bold"><img src={Logo} alt="" className='w-24' /></NavLink>
-          
-
-          {/* Botón para abrir el menú */}
-          <button
-            className="text-4xl text-black cursor-pointer"
-            onClick={toggleMenu}
-          >
-            &#9776; {/* Ícono de menú */}
-          </button>
+    <div>
+      <div className="bg-black/80 flex item-center justify-between">
+        <div className='mt-8 ml-20 space-x-10'>
+          <a href="" className='px-4 py-2 border border-white-500 rounded-lg bg-black text-white-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md'>Reservas</a>
+          <a href="" className='px-4 py-2 border border-white-500 rounded-lg bg-black text-white-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md'>Sobre nosotros</a>
+          <a href="" className='px-4 py-2 border border-white-500 rounded-lg bg-black text-white-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md'>Barcos</a>
         </div>
+        
+        <div className=''>
+          <img src={logo} alt="" className='w-32 mr-80'/>
+        </div>
+        
+
+        <div className='mt-8 mr-20'>
+          <a href="" className='px-4 py-2 border border-blue-500 rounded-lg bg-white text-blue-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md'>Contacto</a>
+        </div>
+        
       </div>
+    </div>
+  )
+}
 
-      {/* Menú lateral que se abre al hacer clic */}
-      <nav className={`fixed inset-0 z-40 transition-transform duration-500 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className="relative w-full h-full">
-          {/* Botón para cerrar el menú */}
-          <button
-            className="absolute top-6 right-6 text-black text-4xl"
-            onClick={toggleMenu}
-          >
-            &#x2715; {/* Ícono de cerrar */}
-          </button>
-
-          {/* Contenedor principal del menú */}
-          <div className="main_menu  flex bg-white/10 h-full gap-48   justify-center items-center">
-            <div className='flex flex-col gap-10 text-white'>
-              <NavLink className="menu_link main text-7xl" to={"/"} itemProp="url">Home</NavLink>
-              <NavLink className="menu_link main text-7xl" to={"/sobre-nosotros"} itemProp="url">Sobre nosotros</NavLink>
-              <NavLink className="menu_link main text-7xl" to={"/Contacto"} itemProp="url">Contacto</NavLink>
-              <NavLink className="menu_link main text-7xl" to={"/alquileres"} itemProp="url">Reservar</NavLink>
-            </div>
-            <img src={Logo} alt="" />
-
-          </div>
-        </div>
-      </nav>
-    </>
-  );
-};
-
-export default Navbar;
+export default Navbar
