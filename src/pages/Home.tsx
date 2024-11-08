4010e23e94c58fb60319dbd6dc148d53d4274c63
ertiti11 <vatitiprieto11@gmail.com>
2024-11-08 14:21:16 +0100

    fix confilcts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,8 @@ import Hero from '../components/Hero'
 import ImageSlider from '../components/ImageSlider'
 import ImageScroll from '../components/ImageScroll'
 import Numbers from '../components/Numbers'
-import Features from '../components/Features'
-import BoatList from '../components/BoatList'
-import Activities from '../components/Activities'
-import Testimonials from '../components/Testimonials'
 import CallToAction from '../components/CallToAction'
-import Contact from '../components/Contact'
+import Contact from '../pages/Contact'
 
 const Home = () => {
   return (
