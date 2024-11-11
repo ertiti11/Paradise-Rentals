cd2458b5835af74373ffaac334e834b2cc09056f
ertiti11 <vatitiprieto11@gmail.com>
2024-11-11 03:46:42 +0100

    Agregar framer-motion y react-intersection-observer; actualizar componente ImageScroll para animaciones y manejo de scroll

--- src/components/ImageScroll.tsx
+++ src/components/ImageScroll.tsx
@@ -1,24 +1,51 @@
-import Caribe from '../assets/caribean.webp'
-import Crucero from '../assets/crucero.webp'
-
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import Caribe from '../assets/caribean.webp';
+import Crucero from '../assets/crucero.webp';
 
 const ImageScroll = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const { ref, inView } = useInView({ threshold: 0.9 });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="flex justify-between">
-      <div className='ml-20 py-2'>
-        <img src={Caribe} alt="imagen 1" className='h-[280px] w-[500px] animate-fadeIn' />
-      </div>
-      <div className='mt-[100px]'>
-        <h1 className="text-4xl">Visita el caribe</h1>
-        <div className='mt-[40px] ml-[60px]'>
-          <a href="" className='px-4 py-2 border border-white-500 rounded-lg bg-black text-white-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md mt-[10px]'>Reservar</a>
+    <div ref={ref} className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+      <motion.div
+        className="absolute transform -translate-x-full z-10"
+        animate={{ x: Math.min(scrollY * 0.5, window.innerWidth / 4) }}
+        transition={{ type: 'tween', duration: 0.5 }}
+      >
+        <img src={Caribe} alt="imagen 1" className="h-[400px] w-[300px]" />
+      </motion.div>
+      <motion.div
+        className="absolute transform translate-x-full z-10"
+        animate={{ x: Math.max(-scrollY * 0.5, -window.innerWidth / 4) }}
+        transition={{ type: 'tween', duration: 1 }}
+      >
+        <img src={Crucero} alt="imagen 2" className="h-[400px] w-[300px]" />
+      </motion.div>
+      <div className="relative z-0 text-center">
+        <h1 className="text-4xl text-white">Visita el caribe</h1>
+        <div className="mt-4">
+          <a
+            href="#"
+            className="px-4 py-2 border border-white-500 rounded-lg bg-black text-white-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 shadow-md"
+          >
+            Reservar
+          </a>
         </div>
       </div>
-      <div className='mr-20 py-2'>
-        <img src={Crucero} alt="imagen 2" className='h-[280px] w-[500px] animate-fadeIn' />
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageScroll
+export default ImageScroll;
