e208764397709138b968c479c82fa173eff9086d
ertiti11 <vatitiprieto11@gmail.com>
2024-11-13 21:23:03 +0100

    fixes

--- src/components/Imagen3d.tsx
+++ src/components/Imagen3d.tsx
@@ -1,87 +1,53 @@
-// import React, { useRef, useEffect } from 'react';
-// import { image } from 'framer-motion/client';
-import Imagen from "../assets/image-6.jpg";
-import { useEffect } from "react";
-import { useRef } from "react";
+"use client";
 
-const Imagen3d = () => {
-  const imageRef = useRef(null);
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import { Link } from "react-router-dom";
 
-  useEffect(() => {
-    const el = imageRef.current;
-    if (!el) return;
-
-    const handleMouseMove = (evt) => {
-      const { layerX, layerY } = evt;
-      const { clientWidth: width, clientHeight: height } = el;
-
-      const yRotation = ((layerX - width / 2) / width) * 15;
-      const xRotation = ((layerY - height / 2) / height) * 15;
-
-      el.style.transform = `
-        perspective(500px)
-        scale(1.1)
-        rotateX(${xRotation}deg)
-        rotateY(${yRotation}deg)`;
-    };
-
-    const handleMouseOut = () => {
-      el.style.transform = `
-        perspective(500px)
-        scale(1)
-        rotateX(0)
-        rotateY(0)`;
-    };
-
-    el.addEventListener("mousemove", handleMouseMove);
-    el.addEventListener("mouseout", handleMouseOut);
-
-    // Cleanup event listeners on component unmount
-    return () => {
-      el.removeEventListener("mousemove", handleMouseMove);
-      el.removeEventListener("mouseout", handleMouseOut);
-    };
-  }, []);
-
+export default function Imagen3d() {
   return (
-    <div className="flex-col">
-      <div className="ml-[770px]">
-        <h1 className="text-4xl ml-9 font-bold">Costa</h1>
-        <h1 className="text-4xl font-bold">Concordia</h1>
-        <br />
-        <br />
-      </div>
-      <div
-        className="w-[full] h-[500px] flex justify-center items-center"
-        ref={imageRef}
-      >
-        <img
-          src={Imagen}
-          alt="imagen"
-          className="w-[500px] h-[400px] rounded-lg"
-        />
-
-        <img
-          src={Imagen}
-          alt=""
-          className="absolute w-[300px] h-[200px] mr-[500px] object-cover z-10 rounded-lg"
-        />
-        <img
-          src={Imagen}
-          alt=""
-          className="absolute w-[300px] h-[200px] ml-[500px] mb-[300px] object-cover z-[-10] rounded-lg"
-        />
-        <img
-          src={Imagen}
-          alt=""
-          className="absolute w-[300px] h-[200px] ml-[500px] mt-[300px] mr-[100px] object-cover z-[10] rounded-lg"
-        />
-      </div>
-      <br />
-      <br />
-      <br />
-    </div>
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <img
+            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
   );
-};
-
-export default Imagen3d;
+}
